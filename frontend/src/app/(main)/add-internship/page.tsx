de5060a7737eaafd0a6a6d61935684a2ddd1b44d
Veeral <veeralnarang999@gmail.com>
2025-09-12 01:18:32 +0530

    -

--- frontend/src/app/(main)/add-internship/page.tsx
+++ frontend/src/app/(main)/add-internship/page.tsx
@@ -1,5 +1,14 @@
+import PageHeading from '@/components/PageHeading';
 import React from 'react';
 
 export default function AddInternshipPage() {
-  return <div>AddInternshipPage</div>;
+  return (
+    <section>
+      <div className="container flex flex-col gap-4 md:gap-6">
+        <PageHeading subText="Add a new government internship opportunity.">
+          Create new Internship
+        </PageHeading>
+      </div>
+    </section>
+  );
 }
--- frontend/src/app/(main)/submit-application/page.tsx
+++ frontend/src/app/(main)/submit-application/page.tsx
@@ -1,5 +1,14 @@
+import PageHeading from '@/components/PageHeading';
 import React from 'react';
 
 export default function SubmitApplicationPage() {
-  return <div>SubmitApplicationPage</div>;
+  return (
+    <section>
+      <div className="container flex flex-col gap-4 md:gap-6">
+        <PageHeading subText="Apply for a government internship opportunity">
+          Submit Application
+        </PageHeading>
+      </div>
+    </section>
+  );
 }
