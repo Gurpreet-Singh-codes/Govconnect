bcc32feb1562ff7be7db42dc85794a4f996ee60c
Veeral <veeralnarang999@gmail.com>
2025-09-12 13:00:18 +0530

    fixed dashboard values

--- frontend/src/app/(main)/dashboard/page.tsx
+++ frontend/src/app/(main)/dashboard/page.tsx
@@ -2,12 +2,26 @@ import Card from '@/components/dashboard/Card';
 import PageHeading from '@/components/PageHeading';
 import { Briefcase, CircleCheckBig, Clock, Users } from 'lucide-react';
 import React from 'react';
-import { dummyDashboardData, recentApplicantsDummyData } from '@/lib/DummyData';
+import { dummyDashboardData, dummyInternships, recentApplicantsDummyData } from '@/lib/DummyData';
 import ApplicationStatusChart from '@/components/charts/ApplicationStatusChart';
 import FeedbackSentimentPieChart from '@/components/charts/FeedbackSentimentChart';
 import RecentApplicantItem from '@/components/dashboard/RecentApplicantItem';
 
 export default function DashboardPage() {
+  const pendingApplications =
+    dummyDashboardData.statusDistribution.find((stat) => stat.status === 'pending')?.count || 0;
+  const underReviewApplications =
+    dummyDashboardData.statusDistribution.find((stat) => stat.status === 'under review')?.count ||
+    0;
+  const acceptedApplications =
+    dummyDashboardData.statusDistribution.find((stat) => stat.status === 'accepted')?.count || 0;
+  const rejectedApplications =
+    dummyDashboardData.statusDistribution.find((stat) => stat.status === 'rejected')?.count || 0;
+  //
+  const totalApplications =
+    pendingApplications + underReviewApplications + acceptedApplications + rejectedApplications;
+
+  const openPositions = dummyInternships.length;
   return (
     <section>
       <div className="container flex flex-col gap-4 md:gap-6">
@@ -19,7 +33,7 @@ export default function DashboardPage() {
           <Card className="flex items-center justify-between gap-2">
             <div>
               <h3 className="text-sm font-medium text-gray-600">Total Applications</h3>
-              <p className="mt-1 text-3xl font-bold">0</p>
+              <p className="mt-1 text-3xl font-bold">{totalApplications}</p>
             </div>
             <div className="flex aspect-square size-10 items-center justify-center rounded-lg bg-blue-100 text-blue-600">
               <Users size={20} />
@@ -29,7 +43,7 @@ export default function DashboardPage() {
           <Card className="flex items-center justify-between gap-2">
             <div>
               <h3 className="text-sm font-medium text-gray-600">Open Positions</h3>
-              <p className="mt-1 text-3xl font-bold">0</p>
+              <p className="mt-1 text-3xl font-bold">{openPositions}</p>
             </div>
             <div className="flex aspect-square size-10 items-center justify-center rounded-lg bg-purple-100 text-purple-600">
               <Briefcase size={20} />
@@ -39,7 +53,7 @@ export default function DashboardPage() {
           <Card className="flex items-center justify-between gap-2">
             <div>
               <h3 className="text-sm font-medium text-gray-600">Under Review</h3>
-              <p className="mt-1 text-3xl font-bold">0</p>
+              <p className="mt-1 text-3xl font-bold">{underReviewApplications}</p>
             </div>
             <div className="flex aspect-square size-10 items-center justify-center rounded-lg bg-yellow-100 text-yellow-600">
               <Clock size={20} />
@@ -49,7 +63,7 @@ export default function DashboardPage() {
           <Card className="flex items-center justify-between gap-2">
             <div>
               <h3 className="text-sm font-medium text-gray-600">Accepted</h3>
-              <p className="mt-1 text-3xl font-bold">0</p>
+              <p className="mt-1 text-3xl font-bold">{acceptedApplications}</p>
             </div>
             <div className="flex aspect-square size-10 items-center justify-center rounded-lg bg-green-100 text-green-600">
               <CircleCheckBig size={20} />
